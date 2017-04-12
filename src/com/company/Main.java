61e58724b0e889fb1dbb283ddc61936ada472f71
js456 <justins4@comcast.net>
2017-04-12 10:04:34 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.io.FileReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //need to make the filereader stuff.
+        //the data is separated by a tab "\t"
+        //there is a space between the + and grate (ex. + .5 + 1 + 2 + .5 + .5 + 1 + .5 + )
+
+        FileReader fr = new FileReader("names.txt");
+        
+    }
+}
