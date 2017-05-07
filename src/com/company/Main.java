245854ca264eb6572903b7b54c2180ec5cb1f6b6
jsmith0 <justins4@comcast.net>
2017-05-07 10:02:40 -0400

    getting there

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,32 +16,58 @@ public class Main {
         BufferedReader br = new BufferedReader(new FileReader("tester.txt"));
 
         ArrayList<String> line = new ArrayList();
-       ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
         ArrayList<String> nums = new ArrayList<>();
         ArrayList<String> grade1 = new ArrayList<>();
         ArrayList<String> grade2 = new ArrayList<>();
         ArrayList<String> gradeTotal = new ArrayList<>();
+        ArrayList<String> working = new ArrayList();
 
         Scanner sc = new Scanner("tester.txt");
 
         int n = 0;
         int p = 0;
 
-        String w = sc.nextLine();
+        String filename = "tester.txt";
+        File file = new File(filename);
+        Scanner sc1 = new Scanner(file);
 
+        while (sc1.hasNextLine()) {
+            String nxt = sc1.nextLine();
+            String e[] = nxt.split(":");
+            String name = e[0];
+            String secretnum = e[1];
+            String fr1 = e[2];
+            names.add(name);
+            nums.add(secretnum);
 
+            //fixing up fr1
+            String redone = "";
+            for (int i = 0; i <fr1.length(); i++){
+                char letter = fr1.charAt(i);
+                int asc = (int)letter;
+                if (asc == 44 || asc == 46 || asc == 48 || asc == 49 || asc == 50 || asc == 51 || asc == 52
+                        || asc == 53 || asc == 54 || asc == 55 || asc == 56 || asc == 57){
+                    String r = Character.toString(letter);
+                    redone += letter;
 
-        while (sc.hasNextLine()){
-                String[] temp;
-                temp = w.split(":");
-                names.add(temp[0]);
-                nums.add(temp[1]);
-                grade1.add(temp[2]);
-                w = (sc.nextLine());
-                n++;
+                }
+            }
+            String []almost = redone.split(",");
+            for (int i = 0; i < almost.length; i++){
+                String temp = almost[i];
+                String temp2 = "";
+                for (int v = 0; v < temp.length(); v++){
+                    char u = temp.charAt(v);
+                    if (u != 43){
+                     temp2 += u;
+                    }
+                }
+                almost[i] = temp2;
+
+            }
+            n++;
         }
-
-
         System.out.println(names);
         System.out.println(nums);
         System.out.println(grade1);
