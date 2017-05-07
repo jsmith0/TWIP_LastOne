1ad32b1f3c114255f06753baab52dd2a277157d3
jsmith0 <justins4@comcast.net>
2017-05-07 10:27:09 -0400

    getting there

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,8 +18,8 @@ public class Main {
         ArrayList<String> line = new ArrayList();
         ArrayList<String> names = new ArrayList<>();
         ArrayList<String> nums = new ArrayList<>();
-        ArrayList<String> grade1 = new ArrayList<>();
-        ArrayList<String> grade2 = new ArrayList<>();
+        ArrayList<Double> grade1 = new ArrayList<>();
+        ArrayList<Double> grade2 = new ArrayList<>();
         ArrayList<String> gradeTotal = new ArrayList<>();
         ArrayList<String> working = new ArrayList();
 
@@ -38,6 +38,7 @@ public class Main {
             String name = e[0];
             String secretnum = e[1];
             String fr1 = e[2];
+            String fr2 = e[3];
             names.add(name);
             nums.add(secretnum);
 
@@ -48,12 +49,12 @@ public class Main {
                 int asc = (int)letter;
                 if (asc == 44 || asc == 46 || asc == 48 || asc == 49 || asc == 50 || asc == 51 || asc == 52
                         || asc == 53 || asc == 54 || asc == 55 || asc == 56 || asc == 57){
-                    String r = Character.toString(letter);
                     redone += letter;
 
                 }
             }
             String []almost = redone.split(",");
+            double fr1Score = 0;
             for (int i = 0; i < almost.length; i++){
                 String temp = almost[i];
                 String temp2 = "";
@@ -62,14 +63,50 @@ public class Main {
                     if (u != 43){
                      temp2 += u;
                     }
+
                 }
                 almost[i] = temp2;
+                double soClose = Double.parseDouble(almost[i]);
+                fr1Score += soClose;
 
             }
+            grade1.add(fr1Score);
+
+
+            //same as fr1 but needs to be for fr2... adding 44 to all variables... made fr1 -> fr2
+            String redone44 = "";
+            for (int i = 0; i <fr2.length(); i++){
+                char letter44 = fr2.charAt(i);
+                int asc44 = (int)letter44;
+                if (asc44 == 44 || asc44 == 46 || asc44 == 48 || asc44 == 49 || asc44 == 50 || asc44 == 51 || asc44 == 52
+                        || asc44 == 53 || asc44 == 54 || asc44 == 55 || asc44 == 56 || asc44 == 57){
+                    redone44 += letter44;
+
+                }
+            }
+            String []almost44 = redone44.split(",");
+            double fr2Score = 0;
+            for (int i = 0; i < almost44.length; i++){
+                String temp44 = almost44[i];
+                String temp244 = "";
+                for (int v = 0; v < temp44.length(); v++){
+                    char u = temp44.charAt(v);
+                    if (u != 43){
+                        temp244 += u;
+                    }
+
+                }
+                almost44[i] = temp244;
+                double soClose44 = Double.parseDouble(almost44[i]);
+                fr2Score += soClose44;
+
+            }
+            grade2.add(fr2Score);
             n++;
         }
         System.out.println(names);
         System.out.println(nums);
         System.out.println(grade1);
+        System.out.println(grade2);
     }
 }
